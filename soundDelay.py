58226b7f7a5fe1e104713294b8c3086b2cbf7cb6
Hannah Cussen <hcussen6@gmail.com>
2020-11-21 23:17:32 -0700

    debugging

--- soundDelay.py
+++ soundDelay.py
@@ -10,21 +10,30 @@ st.title('Sound Delay')
 
 # coordinate scheme: front sideline intersects 50 = (0, 0, 0)
 
+# col0, col1 = st.beta_columns(2)
+# # allow user to set height and distance of box
+# box_y, box_z = 0, 0
+# with col0:
+
+# with col1:
+
+
+box_y = st.number_input("Box Height (yds)", min_value=0, value=0, step=1)
+box_z = st.number_input(
+    "Box Distance from front sideline (yds)", min_value=0, value=0, step=1)
+
 # allow user to choose location of drums and hornline
+st.write('### Drums Position')
 drums_x = st.slider('Drums L/R Position', min_value=-50, max_value=50,
                     value=0, step=1)
 drums_y = st.slider('Drums Front/Back Position', min_value=0, max_value=53,
                     value=0, step=1)
-
+st.write('### Horns Position')
 horns_x = st.slider('Horns L/R Position', min_value=-50, max_value=50,
                     value=0, step=1)
 horns_y = st.slider('Horns Front/Back Position', min_value=0, max_value=53,
                     value=0, step=1)
 
-# allow user to set height and distance of box
-box_y = st.number_input("Box Height (yds)", min_value=0.0)
-box_z = st.number_input(
-    "Box Distance from front sideline (yds)", min_value=0.0)
 
 # allow user to set tempo in beats/min
 tempo = st.slider('Tempo (BPM)', min_value=0, max_value=300,
@@ -57,19 +66,20 @@ note_keys = list(notes.keys())
 adjustment = ''
 for i in range(len(note_keys) - 1):
     if discrepancy_percentage_of_beat > note_keys[0]:
-        adjustment = "more than a half note"
-    elif discrepancy_percentage_of_beat >= note_keys[i] and discrepancy_percentage_of_beat < note_keys[i + 1]:
+        adjustment = "more than a half"
+    elif discrepancy_percentage_of_beat <= note_keys[i] and discrepancy_percentage_of_beat > note_keys[i + 1]:
         adjustment = notes[note_keys[i]]
-    elif discrepancy_in_seconds < note_keys[-1]:
-        adjustment = "less than a sixty-fourth note"
+    elif discrepancy_percentage_of_beat < note_keys[-1]:
+        adjustment = "less than a sixty-fourth"
 
 ahead_or_behind = 'ahead' if drums_to_box < horns_to_box else 'behind'
 
-st.write('### Discrepancy as in seconds')
+# display info to user
+st.write('### Discrepancy in seconds')
 st.write(discrepancy_in_seconds)
 
 st.write('### Discrepancy as a percentage of the beat')
 st.write(discrepancy_percentage_of_beat)
 
 st.write('### Adjustment needed')
-st.write(f'{adjustment} {ahead_or_behind}')
+st.write(f'{adjustment} note {ahead_or_behind}')
--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ import math
 
 def distance_to_box(marcher_x, marcher_y, box_y, box_z):
     del_x = marcher_x
-    del_y = marcher_y + abs(box_y)
+    del_y = abs(marcher_y) + abs(box_y)
     del_z = box_z
 
-    return math.sqrt(del_x ** 2 + del_x ** 2 + del_z ** 2)
+    return math.sqrt(del_x ** 2 + del_y ** 2 + del_z ** 2)
